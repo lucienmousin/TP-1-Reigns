e19d59f5e79ea5618e10db4f56e89f5ecc11e1d8
Kirite11111 <eliott.garnier2810@gmail.com>
2023-02-20 14:26:17 +0100

    Personnage.java simplification du if/else

--- src/main/Personnage.java
+++ src/main/Personnage.java
@@ -67,18 +67,14 @@ public class Personnage {
      * @return true si le jeu est fini, false sinon
      */
     public boolean finDuJeu(){
-        if(jaugeClerge.getValeur()<=0
-                || jaugeClerge.getValeur()>=50
-                || jaugePeuple.getValeur()<=0
-                || jaugePeuple.getValeur()>=50
-                || jaugeArmee.getValeur()<=0
-                || jaugeArmee.getValeur()>=50
-                || jaugeFinance.getValeur()<=0
-                || jaugeFinance.getValeur()>=50){
-            return true;
-        }else{
-            return false;
-        }
+        return jaugeClerge.getValeur() <= 0
+                || jaugeClerge.getValeur() >= 50
+                || jaugePeuple.getValeur() <= 0
+                || jaugePeuple.getValeur() >= 50
+                || jaugeArmee.getValeur() <= 0
+                || jaugeArmee.getValeur() >= 50
+                || jaugeFinance.getValeur() <= 0
+                || jaugeFinance.getValeur() >= 50;
     }
 
     /**
